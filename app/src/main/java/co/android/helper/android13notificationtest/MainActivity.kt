72b0876a23536b7e974a6d6eec6a8c983cc10fd2
avinash <avisingh736@gmail.com>
2022-08-09 18:55:56 +0530

    Perform notification permission testing for android 13.

---
+++ app/src/main/java/co/android/helper/android13notificationtest/MainActivity.kt
@@ -0,0 +1,89 @@
+package co.android.helper.android13notificationtest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btn: Button
+    lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.btn)
+
+        btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                    notificationPermission()
+                }
+            }
+        }
+
+        requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                /**
+                 * You can observe permission grant status
+                 * */
+                Log.d("requestPermission", "Permission Grant Status: $isGranted")
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun notificationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                /**
+                 * You do not need to handle anything, if user granted the permission
+                 * */
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                /**
+                 * If user denied initially, this code block will run
+                 * */
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
+                }
+            }
+            else -> {
+                /**
+                 * Initially this code block will run
+                 * */
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            /**
+             * You do not need to handle anything, if user granted the permission
+             * */
+        }
+    }
+}
